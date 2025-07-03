1919687c7983f6ac26ec3d39acb0c8a72f2aa174
lobeiramartin <mglobeira@gmail.com>
2025-07-02 21:19:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="w-100 d-flex flex-column align-items-center">
       <NavBar />
-      <ItemListContainer mensaje="¡Bienvenido a nuestra tienda online!" />
+      <ItemListContainer mensaje="¡Bienvenido a nuestra distribuidora online!" />
     </div>
   );
 }
